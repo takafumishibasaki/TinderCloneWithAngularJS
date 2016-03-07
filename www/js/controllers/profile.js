1001e5eb604d3efcbe0df000b070e1d30e40831b
Takafumi Shibasaki <tkfmsb@gmail.com>
2016-03-07 14:36:56 +0900

    first commit

---
+++ www/js/controllers/profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.controller('ProfileCtrl', function(profile, about, images) {
+  var prof = this;
+
+  prof.currentUser = profile;
+  prof.about = about;
+  prof.images = images;
+});
